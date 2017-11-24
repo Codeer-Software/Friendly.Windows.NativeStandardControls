64845369755b62b4b57eee926c7a5030034ca148
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-11-24 19:20:20 +0900

    version up.

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,4 +1,4 @@
 rd /s /q "../ReleaseBinary"
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../Codeer.Friendly.Windows.NativeStandardControls/Codeer.Friendly.Windows.NativeStandardControls.sln" /rebuild Release
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../Codeer.Friendly.Windows.NativeStandardControls/Codeer.Friendly.Windows.NativeStandardControls.sln" /rebuild Release-English
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../Codeer.Friendly.Windows.NativeStandardControls/Codeer.Friendly.Windows.NativeStandardControls.sln" /rebuild Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../Codeer.Friendly.Windows.NativeStandardControls/Codeer.Friendly.Windows.NativeStandardControls.sln" /rebuild Release-English
 nuget pack friendly.windows.nativestandardcontrols.nuspec
