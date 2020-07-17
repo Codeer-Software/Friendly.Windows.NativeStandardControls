6e06a58bfba397ac52980a1e0c435eae741bf117
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2020-07-17 14:10:57 +0900

    Add WindowControlGenerator

---
+++ Project/Codeer.Friendly.Windows.NativeStandardControls.Generator/WindowControlGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Codeer.TestAssistant.GeneratorToolKit;
+
+namespace Codeer.Friendly.Windows.NativeStandardControls.Generator
+{
+    /// <summary>
+    /// For key mouse capture.
+    /// </summary>
+    [CaptureCodeGenerator("Codeer.Friendly.Windows.Grasp.WindowControl")]
+    public class WindowControlGenerator : CaptureCodeGeneratorBase
+    {
+        /// <summary>
+        /// nothing.
+        /// </summary>
+        protected override void Attach() { }
+
+        /// <summary>
+        /// nothing.
+        /// </summary>
+        protected override void Detach() { }
+    }
+}
